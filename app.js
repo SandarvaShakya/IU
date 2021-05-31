38ae00323054abf4f341182ab529d8a634a09a85
SandarvaShakya <sandarvashakya34@gmail.com>
2021-05-31 21:40:00 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav--toggle");
+});
